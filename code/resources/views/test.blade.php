14389ba5dae63d8427e97b9054c38ce5a46b6ef8
silvanus <silvanusprasetyo@gmail.com>
2025-10-06 13:53:58 +0700

    INITIAL COMMIT

---
+++ code/resources/views/test.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1>Testing Laravel + Tailwindcss</h1>
+
+    <p class="h3 --font-jakarta">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum voluptates amet, ut odio distinctio fugit reprehenderit veritatis, unde nihil facilis autem. Harum molestias expedita esse voluptates corrupti eos repellat adipisci.</p>
+    <div class="h1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quod eum enim vitae fuga, nobis, accusantium et repudiandae praesentium, dolorem in incidunt. Quae, hic inventore. Natus deserunt beatae harum placeat.</div>
+</body>
+</html>
